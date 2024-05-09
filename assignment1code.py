9267b182c155b1907ba884c6b51711cc47862e1d
Andy <109981829+andyallard@users.noreply.github.com>
2024-05-09 01:50:27 -0600

    Add screenshot showing success

--- assignment1code.py
+++ assignment1code.py
@@ -1,10 +1,14 @@
 # ENSF 692 Spring 2024
 # Assignment 1 Starter Code
-# Add your name here!
+# Andy Allard
 
-studentName = "Sarah Shah"  # Change the contents of this variable to your name (leave the quotation marks).
+studentName = "Andy Allard"  # Change the contents of this variable to your name (leave the quotation marks).
 
 print("Hello, World!")
 print("My name is " + studentName + " and I am a student in ENSF 692.")
 
 # print your best skill here
+print("My best skill is realizing that I should offer to help "
+      "someone do something (like cleaning up after a meal) "
+      "and then offering, but then they say 'No thanks, I'm almost done.' "
+      " I swear it's not on purpose, but it's too convenient to be believable. ")
